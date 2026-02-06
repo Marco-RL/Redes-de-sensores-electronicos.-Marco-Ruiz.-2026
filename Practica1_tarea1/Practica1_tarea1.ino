bc242c779abf293b7e5ec1b239b5e4e8fb3efd4e
Marco Ruiz <marcoruizlopez@gmail.com>
2026-02-06 13:54:32 +0100

    Eliminación de comentarios

--- Practica1_tarea1/Practica1_tarea1.ino
+++ Practica1_tarea1/Practica1_tarea1.ino
@@ -1,14 +1,16 @@
 void setup() {
-  // initialize serial communication at 9600 bits per second:
+
   Serial.begin(115200);
 }
 
-int raw_ADC;
-// the loop function runs over and over again forever
+uint32_t raw_ADC;
+uint32_t voltaje_ADC;
+
 void loop() {
 
   raw_ADC= analogRead(A0);
+  voltaje_ADC = raw_ADC * 3300/1024;
   
-  Serial.println(raw_ADC);
+  Serial.println(voltaje_ADC);
   delay(1);  
 }
